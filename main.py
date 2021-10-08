cebaf80ede0569d897abffabb27d6a5e232fa541
shayanekat <shaykatfsx@gmail.com>
2021-10-08 09:40:05 +0200

    first python files setup

---
+++ main.py
@@ -0,0 +1,16 @@
+""" 
+Shayanekat
+
+main script that will contain all algorithms of image processing.
+"""
+
+from tkinter import *
+from PIL import Image
+import numpy as np
+
+# %% BACKEND
+
+# %% FRONTEND
+root = Tk()
+
+root.mainloop()
---
+++ tests/resizing.py
@@ -0,0 +1,16 @@
+""" 
+Shayanekat
+
+test script for resizing an image (only reducing it's size).
+"""
+
+from tkinter import *
+from PIL import Image
+import numpy as np
+
+# %% BACKEND
+
+# %% FRONTEND
+root = Tk()
+
+root.mainloop()
