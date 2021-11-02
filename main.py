dfba73b8af34417e61d7c2c8ac618f7907b04871
shayanekat <shaykatfsx@gmail.com>
2021-11-02 11:12:54 +0100

    added resizng frontend

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ root = Tk()
 root.title("image processing")
 
 
-# row 0
+# row 0 : image browser
 l0 = Label(root, text="browse image")
 l0.grid(row=0, column=0)
 
@@ -24,6 +24,17 @@ LST_Types = [("images" , [".png", ".jpg", ".jpeg"])]
 e01 = filedialog.askopenfilename(initialdir = "C:", title = "Select a File", filetypes = LST_Types)
 l01 = Label(root, text=e01)
 l01.grid(row=0, column=1, padx=5, pady=5)
+
+
+# row 1 : resizing
+l1 = Label(root, text="enter an image size that is lower than input image size")
+l1.grid(row=1, column=0, padx=5, pady=5)
+
+e1 = Entry(root)
+e1.grid(row=1, column=1, padx=5, pady=5)
+
+c1 = Checkbutton(root,text="allow resizing")
+
 
 
 root.mainloop()
