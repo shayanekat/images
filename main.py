3dafb0db6957f2332f6651a160af814e44df0032
shayanekat <shaykatfsx@gmail.com>
2021-11-02 11:03:31 +0100

    added image browser t main script

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ main script that will contain all algorithms of image processing.
 """
 
 from tkinter import *
+from tkinter import filedialog
 from PIL import Image
 import numpy as np
 
@@ -12,5 +13,17 @@ import numpy as np
 
 # %% FRONTEND
 root = Tk()
+root.title("image processing")
+
+
+# row 0
+l0 = Label(root, text="browse image")
+l0.grid(row=0, column=0)
+
+LST_Types = [("images" , [".png", ".jpg", ".jpeg"])]
+e01 = filedialog.askopenfilename(initialdir = "C:", title = "Select a File", filetypes = LST_Types)
+l01 = Label(root, text=e01)
+l01.grid(row=0, column=1, padx=5, pady=5)
+
 
 root.mainloop()
